d0090c04ffa264872aa385c1b0720bd16a59758c
Manpreet Singh <designviacode@gmail.com>
2019-01-26 03:22:12 +0530

    merged cordova and create-react-app

---
+++ platforms/ios/www/precache-manifest.54990d5022611111a701edc081d9ba5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9180508670bf47cc0b6c",
+    "url": "./static/js/main.91805086.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "./static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "9180508670bf47cc0b6c",
+    "url": "./static/css/main.9c168fe4.chunk.css"
+  },
+  {
+    "revision": "b4382b64dd831b695f64189e1850897a",
+    "url": "./index.html"
+  }
+];
